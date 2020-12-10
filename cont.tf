00ce4d35da5620551e446edf345f454481f3e39e
madhu <ybmadhu707@gmail.com>
2020-12-10 17:53:44 +0000

    added ec2 instance

---
+++ cont.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+region = "ap-south-1"
+}
+# This is a single-line comment.
+resource "aws_instance" "base" {
+ami = "ami-08f63db601b82ff5f"
+instance_type = "t2.micro"
+
+tags = {
+Name = "terraform-test"
+}
+}
+
+
