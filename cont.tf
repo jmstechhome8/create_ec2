814e87447e2764a498eadc7c1ae7dcf576913df7
jmstechhome8 <72117252+jmstechhome8@users.noreply.github.com>
2021-02-18 16:15:08 +0530

    Update cont.tf

--- cont.tf
+++ cont.tf
@@ -3,7 +3,7 @@ region = "ap-south-1"
 }
 # This is a single-line comment.
 resource "aws_instance" "base" {
-ami = "ami-08f63db601b82ff5f"
+ami = "ami-08e0ca9924195beba"
 instance_type = "t2.micro"
 
 tags = {
